99ca4b53302f4043712b4b80ceb1a9e3d02985ce
kentayamada-centennial <155464000+kentayamada-centennial@users.noreply.github.com>
2024-07-19 19:04:52 -0400

    Delete backend/api/Dtos/UpdateAccountDto.cs

--- backend/api/Dtos/UpdateAccountDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace api.Dtos
-{
-    public class UpdateAccountDto
-    {
-        public required string Name { get; set; }
-        public required string Type { get; set; }
-        public required decimal Balance { get; set; }
-    }
-}
